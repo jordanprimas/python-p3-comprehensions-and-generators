ba3e8aec2aa3facb841bb72fca939540ed9c81ec
Jordan Primas <jorprimas20@gmail.com>
2024-01-10 09:53:28 -0800

    completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers_list = [n for n in num_list if n % 2 == 0] 
+    if len(even_numbers_list) == 0:
+        return []
+    else:
+        return even_numbers_list
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation_string = [sentence + "!" for sentence in sentence_list]
+    return(exclamation_string)
